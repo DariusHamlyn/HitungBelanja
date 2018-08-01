6438e75ac203f615d5d7aa33149a668107b33fb4
Aldrick Handinata <aldrickhandinata1@gmail.com>
2018-08-01 20:10:20 +0700

    Added Barang Control

---
+++ Library/Barang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Barang
+    {
+        public string Kode { get; set; }
+        public string Nama { get; set; }
+        public int Jumlah { get; set; }
+        public decimal Harga { get; set; }
+    }
+}
